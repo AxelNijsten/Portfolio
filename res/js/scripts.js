de69e46ae7669cb57a3e3adbdb4a5b34c9bf0cd8
Axel <axelnijsten1@gmail.com>
2019-10-09 15:12:58 +0200

    prototype

--- res/js/scripts.js
+++ res/js/scripts.js
@@ -1,14 +1,16 @@
 
-    $(document).ready(function(){
-        var boxWidth = $(".button-8").width();
-        $(".slide-left").click(function(){
-            $(".button-8").animate({
-                width: 0
-            });
-        });
-        $(".slide-right").click(function(){
-            $(".button-8").animate({
-                width: boxWidth
-            });
-        });
+
+// This translate the nav element to show the selected item
+function translateNav(item) {
+    // If animation is defined, pause it
+    if (navItemsAnimation) navItemsAnimation.pause();
+    // Animate the `translateY` of `nav` to show only the current link
+    navItemsAnimation = anime({
+        targets: navEl,
+        translateY: (item ? -activeIndex * navHeight : 0) + 'px',
+        easing: 'easeOutCubic',
+        duration: 500
     });
+    // Update link on arrows, and disable/enable accordingly if first or last link
+    updateArrows();
+}
