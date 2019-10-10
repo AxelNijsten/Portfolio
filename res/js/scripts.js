38b9a8ec079382870c5709d816f71c27cbc9d30d
Axel <axelnijsten1@gmail.com>
2019-10-10 08:19:21 +0200

    javascript try 1

--- res/js/scripts.js
+++ res/js/scripts.js
@@ -1,16 +1,33 @@
 
+function smoothScroll(target,duration){
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect();
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
 
-// This translate the nav element to show the selected item
-function translateNav(item) {
-    // If animation is defined, pause it
-    if (navItemsAnimation) navItemsAnimation.pause();
-    // Animate the `translateY` of `nav` to show only the current link
-    navItemsAnimation = anime({
-        targets: navEl,
-        translateY: (item ? -activeIndex * navHeight : 0) + 'px',
-        easing: 'easeOutCubic',
-        duration: 500
-    });
-    // Update link on arrows, and disable/enable accordingly if first or last link
-    updateArrows();
+    function animation(currentTime){
+        if(startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0,run);
+        if(timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d){
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (T - 2) - 1) + b;
+    }
+   
+
+requestAnimationFrame(animation);
 }
+
+
+   
+var button-8 = document.querySelector(".works");
+button-8.addEventListener('click', function(){
+    smoothScroll(".works", 1000);
+})
