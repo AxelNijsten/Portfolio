8de0ff5910215769ddc3fdf471fef8c8c37b6062
Axel <axelnijsten1@gmail.com>
2019-10-10 10:35:24 +0200

    animations

--- res/js/scripts.js
+++ res/js/scripts.js
@@ -1,33 +1,5 @@
-
-function smoothScroll(target,duration){
-    var target = document.querySelector(target);
-    var targetPosition = target.getBoundingClientRect();
-    var startPosition = window.pageYOffset;
-    var distance = targetPosition - startPosition;
-    var startTime = null;
-
-    function animation(currentTime){
-        if(startTime === null) startTime = currentTime;
-        var timeElapsed = currentTime - startTime;
-        var run = ease(timeElapsed, startPosition, distance, duration);
-        window.scrollTo(0,run);
-        if(timeElapsed < duration) requestAnimationFrame(animation);
-    }
-
-    function ease(t, b, c, d){
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t + b;
-        t--;
-        return -c / 2 * (t * (T - 2) - 1) + b;
-    }
-   
-
-requestAnimationFrame(animation);
-}
-
-
-   
-var button-8 = document.querySelector(".works");
-button-8.addEventListener('click', function(){
-    smoothScroll(".works", 1000);
+$(document).on('click','.goToSection',function(e){
+    $('html, body').animate({
+        scrollTop: $($(this).attr('data-href')).offset().top
+    }, 1000);//set second as required
 })
